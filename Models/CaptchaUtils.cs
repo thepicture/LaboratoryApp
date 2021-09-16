8d3e348f8e0fe1d54382b8ef076ac5c3af7a411e
thepicture <fuzzyeastern@protonmail.com>
2021-09-16 06:40:08 +0100

    Improve captcha image

--- Models/CaptchaUtils.cs
+++ Models/CaptchaUtils.cs
@@ -1,12 +1,7 @@
 ﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-using System.Reflection;
 using System.Text;
 using System.Windows;
-using System.Windows.Controls;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
 
 namespace LaboratoryApp.Models
 {
@@ -16,16 +11,16 @@ namespace LaboratoryApp.Models
         private static readonly int FONT_SIZE = 20;
         private static readonly int SYMBOLS_COUNT = 4;
         private static readonly int WIDTH = 200;
-        private static readonly int HEIGHT = 200;
+        private static readonly int HEIGHT = 20;
+        private static readonly int MAX_THICKNESS = 5;
         private static int attempts = 0;
         private static string _captcha;
         private static DrawingImage currentCaptcha;
         private static Random random;
 
         /// <summary>
-        /// Notifies the captcha that a login attempt is incorrect and this class must return its state about captcha image.
+        /// Notifies the captcha that a login attempt is incorrect and that this class must return its state about captcha image.
         /// </summary>
-        /// <returns></returns>
         public static bool NotifyAndCheckIfPrepared()
         {
             attempts++;
@@ -52,14 +47,15 @@ namespace LaboratoryApp.Models
             DrawingContext context = visual.RenderOpen();
 
             context.DrawRectangle(BrushUtils.GetRandomBrush(), null, new Rect(0, 0, WIDTH, HEIGHT));
-            context.DrawLine(new Pen(BrushUtils.GetRandomBrush(), random.Next(0, HEIGHT / 16)), new Point(0, random.Next(0, HEIGHT)), new Point(WIDTH, random.Next(0, HEIGHT)));
-            context.DrawLine(new Pen(BrushUtils.GetRandomBrush(), random.Next(0, HEIGHT / 16)), new Point(0, random.Next(0, HEIGHT)), new Point(WIDTH, random.Next(0, HEIGHT)));
 
             for (int i = 0; i < SYMBOLS_COUNT; i++)
             {
-                FormattedText text = new FormattedText(_captcha, System.Globalization.CultureInfo.InvariantCulture, System.Windows.FlowDirection.LeftToRight, new Typeface("Times New Roman"), FONT_SIZE, BrushUtils.GetRandomBrush(), VisualTreeHelper.GetDpi(visual).PixelsPerDip);
-                context.DrawText(text, new System.Windows.Point(WIDTH / SYMBOLS_COUNT * i + random.Next(-WIDTH, WIDTH), HEIGHT / SYMBOLS_COUNT * i + random.Next(-HEIGHT, HEIGHT)));
+                FormattedText text = new FormattedText(_captcha.ToCharArray()[i].ToString(), System.Globalization.CultureInfo.InvariantCulture, FlowDirection.LeftToRight, new Typeface("Times New Roman"), FONT_SIZE + random.Next(-FONT_SIZE / 2, FONT_SIZE / 2), BrushUtils.GetRandomBrush(), VisualTreeHelper.GetDpi(visual).PixelsPerDip);
+                context.DrawText(text, new Point(WIDTH / SYMBOLS_COUNT * i, HEIGHT / SYMBOLS_COUNT * i + random.Next(-HEIGHT / 2, HEIGHT / 2)));
             }
+            context.DrawLine(new Pen(BrushUtils.GetRandomBrush(), random.Next(1, MAX_THICKNESS)), new Point(0, random.Next(0, HEIGHT)), new Point(WIDTH, random.Next(0, HEIGHT)));
+            context.DrawLine(new Pen(BrushUtils.GetRandomBrush(), random.Next(1, MAX_THICKNESS)), new Point(0, random.Next(0, HEIGHT)), new Point(WIDTH, random.Next(0, HEIGHT)));
+
             context.Close();
 
             currentCaptcha = new DrawingImage(visual.Drawing);
@@ -95,7 +91,6 @@ namespace LaboratoryApp.Models
         /// Checks if the captcha is correct.
         /// </summary>
         /// <param name="captcha"></param>
-        /// <returns></returns>
         public static bool ValidateCaptcha(string captcha)
         {
             if (captcha != _captcha)
