c3d0507234274edc2ac9945abc40142a58bf16ca
thepicture <fuzzyeastern@protonmail.com>
2021-09-18 12:11:46 +0100

    Add ServiceOfOrderReportFormer

---
+++ Models/ServiceOfOrderReportFormer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LaboratoryApp.Models
+{
+    class ServiceOfOrderReportFormer : IReportFormer
+    {
+        public void Form(string destinationPath, bool isVisible)
+        {
+            
+        }
+    }
+}
