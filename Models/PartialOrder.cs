cdf30cd4361ce18ba5c085f6010483d23e5a744f
thepicture <fuzzyeastern@protonmail.com>
2021-09-18 11:06:02 +0100

    Add PartialUser, some design to Assistant biological items list

---
+++ Models/PartialOrder.cs
@@ -0,0 +1,9 @@
+﻿using System.Linq;
+
+namespace LaboratoryApp.Models
+{
+    public partial class Order
+    {
+        public string GetPriceSum => ServiceOfOrder.ToList().Sum(s => s.Service.Price).ToString("N2");
+    }
+}
